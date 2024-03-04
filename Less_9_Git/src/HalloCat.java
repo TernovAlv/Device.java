395821a6aab120fbea2ddd960d544d7a4ce921d1
TernovAlv <ternovaalv@gmail.com>
2024-03-04 21:35:21 +0200

    I send 1 Class

--- Less_9_Git/src/HalloCat.java
+++ Less_9_Git/src/HalloCat.java
@@ -1,2 +1,12 @@
+import java.util.Arrays;
+
 public class HalloCat {
+    public static void main(String[] args) {
+        // Задаємо дві  строкові змінни
+        String word1 = "Hello";
+        String word2 = "World";
+        //Виводимо на консоль
+        System.out.println("Hello " + "World");
+
+    }
 }
