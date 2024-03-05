67ec3003f2da56c2b1882af4d3ba887ef6a6a303
TernovAlv <ternovaalv@gmail.com>
2024-03-05 21:19:24 +0200

    Less 2_Task 2

---
+++ Less_2_DataTypes/src/Task_2_String.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Task_2_String {
+    public static void main(String[] args) {
+        //Використовуючи IntelliJ IDEA, створіть проект. Створіть три рядкові змінні та задайте їм значення:
+        //"\nмій рядок 1"
+        //"\tмій рядок 2"
+        //"\aмій рядок 3"
+        //Виведіть значення кожної змінної на екран. Які відмінності ви побачили? Зробіть висновки.
+        String n="рядок 1";
+        String t="рядок 2";
+        String a="рядок 3";
+        System.out.println(n);
+        System.out.println(t);
+        System.out.println(a);
+
+    }
+}
