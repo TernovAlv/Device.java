51b24c5754ab0365bcbfa346323025e41ec010cb
TernovAlv <ternovaalv@gmail.com>
2024-03-05 22:15:19 +0200

    Less_2_Task4 Konstantyne! Ja  розібралась!

---
+++ Less_2_DataTypes/src/Task_4_GreenGrass.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Task_4_GreenGrass {
+    public static void main(String[] args) {
+        // Завдання 4
+        // Використовуючи логічний тип даних, створіть програму, в якій буде логічне твердження, наприклад,
+        // «трава зелена» і поряд виведіть значення змінної, істинний цей вираз або хибний
+        boolean grassGreen=true;
+        boolean nichtGreen=false;
+        System.out.println("Tрава зелена  " + grassGreen);
+        System.out.println("Суха трава зелена  " + nichtGreen);
+    }
+
+}
