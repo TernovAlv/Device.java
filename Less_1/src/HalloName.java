ab2af20c6a657acf196b540df53a8154446bb2b0
TernovAlv <ternovaalv@gmail.com>
2024-03-06 09:23:06 +0200

    Less_2_Task 6 Konstantyne! Розібралась!

---
+++ Less_1/src/HalloName.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class HalloName {
+    public static void main(String[] args) {
+        System.out.println("Hallo,Konstantyne !");
+        System.out.print( "Hallo, ");
+        System.out.print("Druzhische! " );
+    }
+}
---
+++ Less_2_DataTypes/src/Less_2_Task_6_char.java
@@ -0,0 +1,27 @@
+public class Less_2_Task_6_char {
+
+    public static void main(String[] args) {
+        char unicodeД = '\u0414';// Створіть необхідну кількість змінних типу char,
+        char unicodeо = '\u043e';// кожній змінній надайте значення одного символу у форматі UNICODE.
+        char unicodeб = '\u0431';
+        char unicodeр = '\u0440';
+        char unicodeи = '\u0438';
+        char unicodeд = '\u0434';
+        char unicodeе = '\u0435';
+        char unicodeн = '\u043d';
+        char unicodeь = '\u044c';
+        char unicodeComa = '\u002c';
+        char unicodeА = '\u0410';
+        char unicodeл = '\u043b';
+        char unicodeв = '\u0432';
+        char unicodeі = '\u0456';
+        char unicodeа = '\u0430';
+        char unicodeVoskl ='\u0021';
+        // Виведіть у консоль фразу «Добридень, ВАШЕ_ІМ'Я!».
+        System.out.println(""+ unicodeД + unicodeо +unicodeб +unicodeр + unicodeи  + unicodeд + unicodeе + unicodeн +
+                unicodeь  + unicodeComa  + unicodeА + unicodeл + unicodeь + unicodeв + unicodeі + unicodeн + unicodeа  +unicodeVoskl+"");
+    }
+
+
+
+}
